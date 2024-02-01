f08fb86b5e7024633f5c855c2bb88bfc46b6a604
Alexa Vizcaino <146246560+alexavs1991@users.noreply.github.com>
2024-02-01 01:17:24 +0000

    commit

--- src/models.py
+++ src/models.py
@@ -1,29 +1,55 @@
 import os
 import sys
-from sqlalchemy import Column, ForeignKey, Integer, String
+from sqlalchemy import Column, Integer, String, ForeignKey, DateTime
 from sqlalchemy.orm import relationship, declarative_base
 from sqlalchemy import create_engine
 from eralchemy2 import render_er
 
 Base = declarative_base()
 
-class Person(Base):
-    __tablename__ = 'person'
-    # Here we define columns for the table person
-    # Notice that each column is also a normal Python instance attribute.
-    id = Column(Integer, primary_key=True)
-    name = Column(String(250), nullable=False)
+class User(Base):
+    __tablename__ = 'users'
 
-class Address(Base):
-    __tablename__ = 'address'
-    # Here we define columns for the table address.
-    # Notice that each column is also a normal Python instance attribute.
     id = Column(Integer, primary_key=True)
-    street_name = Column(String(250))
-    street_number = Column(String(250))
-    post_code = Column(String(250), nullable=False)
-    person_id = Column(Integer, ForeignKey('person.id'))
-    person = relationship(Person)
+    username = Column(String(50), unique=True, nullable=False)
+    email = Column(String(100), unique=True, nullable=False)
+    password = Column(String(250), nullable=False)
+    posts = relationship('Post', back_populates='user')
+    comments = relationship('Comment', back_populates='user')
+    likes = relationship('Like', back_populates='user')
+
+class Post(Base):
+    __tablename__ = 'posts'
+
+    id = Column(Integer, primary_key=True)
+    caption = Column(String(255), nullable=False)
+    image_url = Column(String(255), nullable=False)
+    created_at = Column(DateTime(timezone=True))
+    user_id = Column(Integer, ForeignKey('users.id'))
+    user = relationship('User', back_populates='posts')
+    comments = relationship('Comment', back_populates='post')
+    likes = relationship('Like', back_populates='post')
+
+class Comment(Base):
+    __tablename__ = 'comments'
+
+    id = Column(Integer, primary_key=True)
+    text = Column(String(255), nullable=False)
+    created_at = Column(DateTime(timezone=True))
+    user_id = Column(Integer, ForeignKey('users.id'))
+    post_id = Column(Integer, ForeignKey('posts.id'))
+    user = relationship('User', back_populates='comments')
+    post = relationship('Post', back_populates='comments')
+
+class Like(Base):
+    __tablename__ = 'likes'
+
+    id = Column(Integer, primary_key=True)
+    user_id = Column(Integer, ForeignKey('users.id'))
+    post_id = Column(Integer, ForeignKey('posts.id'))
+    user = relationship('User', back_populates='likes')
+    post = relationship('Post', back_populates='likes')
+
 
     def to_dict(self):
         return {}
